46280dbf237cf36815eb1fe0c3f6f75e9d05bcdf
Jeremias Queiroga da Costa Araú´jo <jqcaraujo@gmail.com>
2018-03-25 22:08:50 -0300

    Include file
    Commit the project

---
+++ src/main/java/br/com/testAPI/maxMilhas/TesteAPI/TestAPI.java
@@ -0,0 +1,11 @@
+package br.com.testAPI.maxMilhas.TesteAPI;
+
+import org.junit.runner.*;
+
+public class TestAPI {
+
+	public static void main(String args[]) {
+	     JUnitCore.main("br.com.testAPI.maxMilhas.TesteAPI.suitTestAPI");
+	    }
+	
+}
